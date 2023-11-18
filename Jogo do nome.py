f40d6e2f247fdd2e40c8d7b02d7cf90aa9102f62
whatwayy <m68905966@gmail.com>
2023-11-18 16:01:17 -0300

    primeiro jogo em python

---
+++ Jogo do nome.py
@@ -0,0 +1,4 @@
+nome = input('Olá, qual o seu nome?')
+idade = input('Qual a sua idade?')
+print('O seu nome é {} e você tem {} anos, parabéns! \nJogo criado em python'.format(nome, idade). end(' divirta-se com seu novo jogo em python'))
+
