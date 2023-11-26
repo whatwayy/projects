5fe4151c68b1d14b7d364fe3ecbc11bd929cacc6
whatwayy <m68905966@gmail.com>
2023-11-26 15:35:18 -0300

    Conversor de medidas

---
+++ Conversor de medidas.py
@@ -0,0 +1,8 @@
+m1 = float(input('Digite a medida em metros:'))
+c = m1 / 100
+mili = m1 / 1000
+hectô = m1 / 10
+decâ = m1 / 5
+kilô = m1 / 1000
+decí = m1
+print('centímetros {} \n milímetros {} \nhectômetros {} \ndecâmetro {} \nkilômetros \ndecímetros {}'.format(c, mili, hectô, decâ, kilô, decí))
