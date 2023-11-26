9e5bad84f089d6a195d79ed2c3ec3bae9cfb21b7
whatwayy <m68905966@gmail.com>
2023-11-25 21:44:58 -0300

    medir média do aluno

---
+++ Nota do Aluno.py
@@ -0,0 +1,5 @@
+print('Bem Vindo à calculadora da média do aluno')
+nt1 = float(input('Digite a primeira nota: '))
+nt2 = float(input('Digte a segunda nota: '))
+resultado = (nt1 + nt2) / 2
+print('A média do aluno é de {}'.format(resultado))
