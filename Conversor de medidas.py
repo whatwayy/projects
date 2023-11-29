27e5ac199d8242d148fa13f4119b3e3f6847dc54
whatwayy <m68905966@gmail.com>
2023-11-29 20:33:38 -0300

    update

--- Conversor de medidas.py
+++ Conversor de medidas.py
@@ -1,10 +1,5 @@
-m1 = float(input('Digite a medida em metros:'))
-c = m1 ** -2
-mili = m1 ** -3
-hectô = m1 ** 2
-decâ = m1 ** 1
-quilô = m1 ** 3
-decí = m1 ** -1
-print('centímetros {} \n milímetros {} \nhectômetros {} \ndecâmetro {} \nkilômetros \ndecímetros {}'.format(c, mili, hectô, decâ, quilô, decí))
-
-#test
+m = float(input('Digite um valor em metros: '))
+c = m * 100
+mi = m * 1000
+print('Valor convertido:')
+print('Metros: {} \ncentímetros: {} \nmilímetros: {}'.format(m, c, mi))
